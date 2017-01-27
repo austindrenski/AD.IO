ada7d4b7bdd8fe25757d8b5baf6f1c703fcef59b
Austin Drenski <austin.drenski@gmail.com>
2017-01-26 23:51:50 -0500

    Editing AssemblyInfo.cs

--- Properties/AssemblyInfo.cs
+++ Properties/AssemblyInfo.cs
@@ -5,7 +5,7 @@ using System.Runtime.InteropServices;
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("AD.IO")]
-[assembly: AssemblyDescription("Extension library for basic input/output operations.")]
+[assembly: AssemblyDescription("Extension library for common input/output operations.")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("Austin Drenski")]
 [assembly: AssemblyProduct("AD.IO")]
@@ -31,5 +31,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
