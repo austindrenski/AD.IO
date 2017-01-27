ed959a07799cc7c61684d53a9f6f5b557501498a
Austin Drenski <austin.drenski@gmail.com>
2017-01-26 23:43:15 -0500

    Changed templates to do not copy.

---
+++ AD.IO.Tests/AD.IO.Tests/src/ToCapitalizedExtensionsTests.cs
@@ -0,0 +1,23 @@
+﻿using AjdExtensions.IO;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace AjdExtensionsTests
+{
+    [TestClass]
+    public class ToCapitalizedExtensionsTests
+    {
+        [TestMethod]
+        public void ToCapitalizeFirstTest()
+        {
+            // Arrange
+            const string test = "lowercase";
+            const string expected = "Lowercase";
+            
+            // Act
+            string result = test.ToCapitalizeFirst();
+            
+            // Assert
+            Assert.AreEqual(expected, result);
+        }
+    }
+}
